619e5088c7989ae6d7e239516c0e81bdf78ae272
Paweł <37835885+pawelmusial1995@users.noreply.github.com>
2018-07-26 15:48:39 +0200

    adding another JS function on the browser

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
 var scores , roundScore, activePlayer, dice;
 
 scores = [0, 0];
@@ -19,6 +20,28 @@ dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
 
 //querySelector odwolujemy sie do CSSa i mozemy nadac mu własności
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 // or to parse HTML 
-//document.querySelector('current-' + activePlayer).innerHTML = '<em>' + dice + '/<em>' ;
+//document.querySelector('current-' + activePlayer).innerHTML = '<em>' + dice + '/<em>';
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
+
+//hiding elements on webside
+document.querySelector('.dice').style.display = 'none';
+
+
+//how to make anonimus function
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    
+ // 1. Random number 
+    
+    dice = Math.floor(Math.random() * 6) + 1;
+ 
+//  2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+//  3. Update the round score IF the rolled number was NOT 1    
+});
