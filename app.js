11c27356190233bffef4ba57e92d1cd3cbcf1323
Paweł <37835885+pawelmusial1995@users.noreply.github.com>
2018-07-27 14:10:16 +0200

    last amendments

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ GAME RULES:
 
 var scores , roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+
+init();
 
 //dice roll value 0-6
 dice = Math.floor(Math.random() * 6) + 1;
@@ -29,11 +28,7 @@ console.log(dice);
 //hiding elements on webside
 document.querySelector('.dice').style.display = 'none';
 
-// wyzerowanie wyniku
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+
 
 //how to make anonimus function
 
@@ -80,7 +75,10 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
             
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         } else {
+            
             
             //Next player
             nextPlayer(); 
@@ -107,11 +105,37 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
        //document.querySelector('.player-1-panel').classList.add('active');
        
        document.querySelector('.dice').style.display = 'none';
+       
+         
+        
     }
+        
+       document.querySelector('.btn-new').addEventListener('click',init); 
 
 
+    function init() {
+        
+        scores = [0, 0];
+        activePlayer = 0;
+        roundScore = 0;
+        
+        document.querySelector('.dice').style.display = 'none'
+        
+            // wyzerowanie wyniku
+        document.getElementById('score-0').textContent = '0';
+        document.getElementById('score-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.getElementById('name-0').textContent = 'Player 1';
+        document.getElementById('name-1').textContent = 'Player 2';
+        
+        document.querySelector('.player-0-panel').classList.remove('winner');
+        document.querySelector('.player-1-panel').classList.remove('winner');
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.remove('active');
+        document.querySelector('.player-0-panel').classList.add('active');
 
-
+    }
 
 
 
